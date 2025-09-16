240a352625e207c13db3224b7436dd415c17b0b4
its-shubhanshi <itsshubhanshi23@gmail.com>
2025-09-16 16:21:31 +0530

    productivity dashboard updated code

--- DOMProject/script.js
+++ DOMProject/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let allElems = document.querySelectorAll(".elem");
+let fullElemPage = document.querySelectorAll(".fullElem");
+let allfullElemBackBtn = document.querySelectorAll(".back");
+
+allElems.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    //console.log(elem.id);
+    console.log(fullElemPage[elem.id]);
+    fullElemPage[elem.id].style.display = "block";
+  });
+});
+
+allfullElemBackBtn.forEach(function(back){
+    //console.log(allfullElemBackBtn[back]);
+    back.addEventListener("click",function(){
+        //console.log(back.id);
+        fullElemPage[back.id].style.display="none";
+        
+    })
+    
+})
