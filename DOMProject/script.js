f75a8d157146d717d48768c9b3dfc156efe355b9
its-shubhanshi <itsshubhanshi23@gmail.com>
2025-09-19 15:25:16 +0530

    timer page

--- DOMProject/script.js
+++ DOMProject/script.js
@@ -120,3 +120,92 @@ function dailyPlan() {
   });
 }
 dailyPlan();
+
+function motivationalQuote() {
+  let motivationalQuoteContent = document.querySelector(".motivation-2 h1 ");
+  let motivationAuthor = document.querySelector(".motivation-3 h2");
+  async function fetchQuote() {
+    try {
+      let response = await fetch("https://quotes-api-self.vercel.app/quote");
+      let data = await response.json();
+      console.log(data);
+
+      motivationalQuoteContent.innerHTML = data.quote;
+      motivationAuthor.innerHTML = data.author;
+    } catch (err) {
+      console.log("error fetching quote :", err);
+    }
+  }
+  fetchQuote();
+}
+motivationalQuote();
+
+function pomodoroTimer() {
+  let timer = document.querySelector(".pomo-timer h1");
+  let startBtn = document.querySelector(".pomo-timer .start-timer");
+  let pauseBtn = document.querySelector(".pomo-timer .pause-timer");
+  let resetBtn = document.querySelector(".pomo-timer .reset-timer");
+  let session = document.querySelector(".pomodoro-fullpage .session");
+  let isWorkSession = true;
+  let totalSeconds = 25 * 60;
+  let timerInterval = null;
+  function updateTimer() {
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
+    timer.innerHTML = `${String(minutes).padStart("2", "0")}:${String(
+      seconds
+    ).padStart("2", "0")}`;
+  }
+  function startTimer() {
+    clearInterval(timerInterval);
+    if (isWorkSession) {
+      timerInterval = setInterval(() => {
+        if (totalSeconds > 0) {
+          totalSeconds--;
+          updateTimer();
+        } else {
+          isWorkSession = false;
+          clearInterval(timerInterval);
+          timer.innerHTML = "05:00";
+          session.innerHTML = "Take a Break";
+          session.style.backgroundColor = "var(--blue)";
+          totalSeconds = 5 * 60;
+        }
+      }, 1);
+    } else {
+      timerInterval = setInterval(function () {
+        if (totalSeconds > 0) {
+          totalSeconds--;
+          updateTimer();
+        } else {
+          isWorkSession = true;
+          clearInterval(timerInterval);
+          timer.innerHTML = "25:00";
+          session.innerHTML = "Work Session";
+          session.style.backgroundColor = "var(--green)";
+          totalSeconds = 25 * 60;
+        }
+      }, 10);
+    }
+  }
+  function pauseTimer() {
+    console.log("pause");
+    clearInterval(timerInterval);
+  }
+  function resetTimer() {
+    console.log("reset");
+
+    totalSeconds = 25 * 60;
+    clearInterval(timerInterval);
+    // console.log(timerInterval);
+
+    updateTimer();
+  }
+
+  startBtn.addEventListener("click", startTimer);
+  pauseBtn.addEventListener("click", pauseTimer);
+  resetBtn.addEventListener("click", resetTimer);
+}
+
+pomodoroTimer();
