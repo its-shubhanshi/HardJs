a1d74c0281c5d6d4a9ffafe9d54106d89e70c00f
its-shubhanshi <itsshubhanshi23@gmail.com>
2025-09-21 14:16:29 +0530

    goals page

--- DOMProject/script.js
+++ DOMProject/script.js
@@ -209,3 +209,182 @@ function pomodoroTimer() {
 }
 
 pomodoroTimer();
+
+function weatherFunctionality() {
+  let apikey = "8e42a5abcf7148c184c151333233004";
+  let city = "Lucknow";
+
+  let header1Time = document.querySelector(".header1 h1");
+  let header1Date = document.querySelector(".header1 h2");
+  let header2Temp = document.querySelector(".header2 h2");
+  let header2Condition = document.querySelector(".header2 h4");
+  let precipitation = document.querySelector(".header2 .precipitration");
+  let humidity = document.querySelector(".header2 .humidity");
+  let wind = document.querySelector(".header2 .wind");
+
+  let data = null;
+  async function weatherAPICall() {
+    let response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}`
+    );
+    data = await response.json();
+
+    console.log(data);
+
+    header2Temp.innerHTML = `${data.current.temp_c}°C`;
+    header2Condition.innerHTML = `${data.current.condition.text}`;
+    wind.innerHTML = `Wind : ${data.current.wind_kph}%`;
+    humidity.innerHTML = `Humidity : ${data.current.humidity}`;
+    precipitation.innerHTML = `Heat Index : ${data.current.heatindex_c}%`;
+  }
+  weatherAPICall();
+
+  function timeDate() {
+    const totalDaysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "WednesDay",
+      "ThursDay",
+      "Friday",
+      "SaturDay",
+    ];
+    const monthsNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let date = new Date();
+
+    let dayofWeek = totalDaysOfWeek[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let tarik = date.getDate();
+    let month = monthsNames[date.getMonth()];
+    let year = date.getFullYear();
+
+    header1Date.innerHTML = `${tarik} ${month}, ${year}`;
+    if (hours > 12) {
+      header1Time.innerHTML = `${dayofWeek}, ${String(hours - 12).padStart(
+        "2",
+        "0"
+      )}:${String(seconds).padStart("2", "0")} PM `;
+    } else {
+      header1Time.innerHTML = `${dayofWeek}, ${String(hours).padStart(
+        "2",
+        "0"
+      )}:${String(minutes).padStart("2", "0")}:${String(seconds).padStart(
+        "2",
+        "0"
+      )} AM`;
+    }
+  }
+  setInterval(() => {
+    timeDate();
+  }, 1000);
+}
+
+weatherFunctionality();
+
+function changeTheme() {
+  let theme = document.querySelector(".theme");
+  let rootElement = document.documentElement;
+  let flag = 0;
+  theme.addEventListener("click", function () {
+    console.log(flag);
+
+    if (flag == 0) {
+      rootElement.style.setProperty("--pri", "#1f1e1dff");
+      rootElement.style.setProperty("--sec", "#222831");
+      rootElement.style.setProperty("--tri1", "#948979");
+      rootElement.style.setProperty("--tri2", "#a8abb0ff");
+
+      flag = 1;
+    } else if (flag == 1) {
+      rootElement.style.setProperty("--pri", "#F1EFEC");
+      rootElement.style.setProperty("--sec", "#030303");
+      rootElement.style.setProperty("--tri1", "#D4C9BE");
+      rootElement.style.setProperty("--tri2", "#123458");
+      flag = 2;
+    } else if (flag == 2) {
+      rootElement.style.setProperty("--pri", "#00215e");
+      rootElement.style.setProperty("--sec", " #2c4e80");
+      rootElement.style.setProperty("--tri1", "#481402");
+      rootElement.style.setProperty("--tri2", "#ffc55a");
+      flag = 0;
+    }
+  });
+}
+changeTheme();
+
+function dailyGoals() {
+  let currentGoals = [];
+
+  if (localStorage.getItem("currentGoals")) {
+    currentGoals = JSON.parse(localStorage.getItem("currentGoals"));
+  } else {
+    console.log("goals list is empty");
+  }
+
+  function renderGoals() {
+    let allGoals = document.querySelector(".allGoals");
+
+    var sum = "";
+    currentGoals.forEach(function (elem, idx) {
+      //console.log(elem);
+      sum =
+        sum +
+        `<div class="goals">
+              <h5>${elem.goal} <span class=${elem.imp}>imp</span></h5>
+              <p>${elem.details}</p>
+              <button id=${idx}><i class="ri-chat-check-line"></i></button>
+            </div>`;
+    });
+    allGoals.innerHTML = sum;
+    localStorage.setItem("currentGoals", JSON.stringify(currentGoals));
+
+    let markCompletedBtn = document.querySelectorAll(".goals button");
+    markCompletedBtn.forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        //console.log(currentTask[btn.id]);
+        currentGoals.splice(btn.id, 1);
+        renderGoals();
+      });
+    });
+  }
+  renderGoals();
+
+  let form = document.querySelector(".addGoals form");
+  let goalinput = document.querySelector(".addGoals form #goals-input");
+  let goaltextarea = document.querySelector(".addGoals form textarea");
+  let goalCheckBox = document.querySelector("#check");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let goalsInput = goalinput.value.trim();
+    let goalstextArea = goaltextarea.value.trim();
+    let goalscheck = goalCheckBox.checked;
+    currentGoals.push({
+      goal: goalsInput,
+      details: goalstextArea,
+      imp: goalscheck,
+    });
+    console.log(currentGoals);
+    renderGoals();
+    goalinput.value = "";
+    goaltextarea.value = "";
+    goalscheck = false;
+  });
+}
+
+dailyGoals();
