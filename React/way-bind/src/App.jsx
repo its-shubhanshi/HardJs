f6ec055f033cccf6cb5031edd6c189194ebc1a41
its-shubhanshi <itsshubhanshi23@gmail.com>
2025-09-26 17:09:31 +0530

    form handling

---
+++ React/way-bind/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name,setName]=useState("")
+  const [age,setAge]=useState(10)
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const newUSer={uname:name, age:age}
+    console.log(newUSer);
+    
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}  id="nameInput" placeholder="Enter your name"/>
+        <input type="text" value={age} onChange={(e)=>setAge(e.target.value)}  id="ageInput" placeholder="Enter your Age"/>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
